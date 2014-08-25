4ae674bc28a03dfeac279a373939874df3a84366
BillCI <bill@concreteinteractive.com>
2014-08-24 20:46:02 -0700

    things

--- app/admin/perlenspiel_script.rb
+++ app/admin/perlenspiel_script.rb
@@ -10,15 +10,20 @@ ActiveAdmin.register PerlenspielScript do
   end
 
 
-
   show do
-    h3 perlenspiel_script.name
-    pre  type: "syntaxhighlighter",class:  "brush: js" do
-        perlenspiel_script.code
-    end
+    #h3 perlenspiel_script.name
+    attributes_table do
+      row :name
+      row :description
+      row :cod do
+        pre  type: "syntaxhighlighter",class:  "brush: js" do
+          perlenspiel_script.code
+        end
 
-    script do
-      "SyntaxHighlighter.all();"
+        script do
+          "SyntaxHighlighter.all();"
+        end
+      end
     end
   end
 end
